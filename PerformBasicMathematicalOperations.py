e1378661833d26e9eb86aae5e2face974c7bc977
Ayan Banerjee <ayanban4000@gmail.com>
2025-06-17 14:36:27 +0530

    Add files via upload

---
+++ PerformBasicMathematicalOperations.py
@@ -0,0 +1,10 @@
+n1=int(input("Enter the first number: "))
+n2=int(input("Enter the second number: "))
+sum=n1+n2
+print("\nAddition: ",sum)
+sub=n1-n2
+print("Subtraction: ",sub)
+mul=n1*n2
+print("multiplication: ",mul)
+div=n1/n2
+print("division: ",div)
